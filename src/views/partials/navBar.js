0c1b3967b1b2e06cca04ed46e50d5b876f6a9ce4
pallikondan <palli.kdn94@gmail.com>
2020-09-25 13:29:46 +0530

    navbar icons added

--- src/views/partials/navBar.js
+++ src/views/partials/navBar.js
@@ -1,14 +1,33 @@
 import React, {Fragment} from "react";
-
+import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
+import NotificationsNoneOutlinedIcon from '@material-ui/icons/NotificationsNoneOutlined';
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
+import PowerSettingsNewOutlinedIcon from '@material-ui/icons/PowerSettingsNewOutlined';
 
 const TopNavBar = (props) => {
     return (
         <Fragment>
-            <div style={{height: "5vh", padding: "10px", display: "flex", flexDirection: "row-reverse"}}>
+            <div style={{height: "5vh", display: "flex", flexDirection: "row-reverse"}}>
                 <ul style={{listStyleType: "none", padding: 0, margin: 0}}>
-                    <li style={{display: "inline", padding: "0 20px",borderLeft:"1px solid gray", borderRight:"1px solid gray",borderCollapse:"collapse"}}>noti</li>
-                    <li style={{display: "inline", padding: "0 20px",borderLeft:"1px solid gray", borderRight:"1px solid gray",borderCollapse:"collapse"}}>profile</li>
-                    <li style={{display: "inline", padding: "0 20px",borderLeft:"1px solid gray", borderRight:"1px solid gray",borderCollapse:"collapse"}}>logout</li>
+                    <li style={{display: "inline",borderLeft:"1px solid gray", borderRight:"1px solid gray",borderCollapse:"collapse"}}>
+                        <IconButton aria-label="delete">
+                            <Badge variant="dot" invisible={false} overlap="circle" color="primary">
+                                <NotificationsNoneOutlinedIcon />
+                            </Badge>
+                        </IconButton>
+                    </li>
+                    <li style={{display: "inline",borderLeft:"1px solid gray", borderRight:"1px solid gray",borderCollapse:"collapse"}}>
+                        <IconButton aria-label="delete">
+                            <AccountCircleOutlinedIcon />
+                            <span className={'font-gray font-small'}>{'Sachin kumar'}</span>
+                        </IconButton>
+                    </li>
+                    <li style={{display: "inline",borderLeft:"1px solid gray", borderRight:"1px solid gray",borderCollapse:"collapse"}}>
+                        <IconButton aria-label="delete">
+                            <PowerSettingsNewOutlinedIcon />
+                        </IconButton>
+                    </li>
                 </ul>
             </div>
         </Fragment>
