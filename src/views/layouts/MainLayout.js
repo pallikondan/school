d4519093210321a504625cdacb08a044ba5fb642
pallikondan <palli.kdn94@gmail.com>
2020-10-07 12:52:44 +0530

    responsive changes

--- src/views/layouts/MainLayout.js
+++ src/views/layouts/MainLayout.js
@@ -1,24 +1,45 @@
-import React, {Fragment} from "react";
+import React, {Fragment,useRef} from "react";
 import {Container, Row, Col} from "react-bootstrap";
 import SideMenu from "../partials/sideMenu";
 import TopNavBar from "../partials/navBar";
 import './layout.css';
 import AppLogo from "../../assets/logo2.jpg"
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const MainLayout = (props) => {
+    const menuRef = useRef(null);
+    const handleMenu = (e) =>{
+    let isMenuOpen = false;
+    if(!isMenuOpen){
+        menuRef.current.style.display='block'
+    }else {
+        menuRef.current.style.display='none'
+
+    }
+};
     return (
         <Fragment>
             <Container className={'mar_pad_0'} fluid>
                 <Row className={'mar_pad_0'}>
-                    <Col style={{boxShadow:"10px 0 5px -11px #aaa"}} className={'mar_pad_0'} xs={12} sm={3} xl={3} md={3} lg={3}>
+                    <Col style={{boxShadow:"10px 0 5px -11px #aaa"}} className={'mar_pad_0'} xs={12} sm={12} xl={3} md={3} lg={3}>
                         <div style={{background: "#35a8fc",textAlign:'center',padding:"10%"}}>
                         <img style={{borderRadius:'50%'}} width="40%" src={AppLogo}  alt={'MiKids'}/>
                         </div>
-                        <div style={{background: "#ffffff", height: "75vh"}}>
-                            <SideMenu/>
+                        <div style={{background: "#ffffff"}}>
+                            <div className="menu-bar">
+                                    <div style={{float:'right'}}>
+                                        <IconButton onClick={handleMenu} aria-label="menu">
+                                            <MenuIcon className={'btn-white'} />
+                                        </IconButton>
+                                    </div>
+                            </div>
+                            <div ref={menuRef} className="menu-items">
+                                <SideMenu/>
+                            </div>
                         </div>
                     </Col>
-                    <Col className={'mar_pad_0'}>
+                    <Col  xs={12} sm={12} xl={9} md={9} lg={9} className={'mar_pad_0'}>
                         <Row className={'mar_pad_0'}>
                             <Col className={'mar_pad_0'}>
                                 <TopNavBar/>
--- src/views/pages/login/login.js
+++ src/views/pages/login/login.js
@@ -7,7 +7,7 @@ import loginThumbnail from '../../../assets/login_vector_ic.png'
 import TextField from '@material-ui/core/TextField';
 import {Alert} from "react-bootstrap";
 import RoundedLogo from "../../../assets/round logo.jpg"
-
+import './login.css'
 const login = (data,action, history) =>{
     const loginData = new FormData();
     loginData.append('username', data.username);
@@ -26,19 +26,21 @@ const LoginPage = (props) => {
 
         return (
             <React.Fragment>
-                <Col style={{background: "#35a8fc", height: "100vh", padding: "5% 1%"}}>
+                <Col className="side-panel" style={{background: "#35a8fc"}}>
+                    <div style={{height: "100vh",padding:'10vh 0 0 0'}}>
                     <div style={{background: "#35a8fc", textAlign: "center"}}>
-                        <img src={loginThumbnail} height={500} alt={"loginbanner"}/>
+                        <img src={loginThumbnail} height={600} alt={"loginbanner"}/>
                     </div>
                     <div style={{padding: "6%", textAlign: "center"}}>
                         <span className={'font-white'}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</span>
                     </div>
+                    </div>
                 </Col>
                 <Col>
-                    <div>
+                    <div style={{height: "100vh"}}>
                         <Row>
                             <Col style={{textAlign: "center"}}>
-                                <div style={{width: "50%", margin: "auto", marginTop: "35%"}}>
+                                <div  style={{width: "50%", margin: "auto", marginTop: "35vh"}}>
                                 <div style={{textAlign:'center'}}><img style={{borderRadius: '50%'}} width="25%" src={RoundedLogo} alt={'MiKids'}/></div>
                                     <div className={'mar_top_5'}>
                                         <span className={'font-medium font-black font-sub-headding'}>Log in to your account.</span>
